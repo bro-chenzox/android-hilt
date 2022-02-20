f901cd92fec5814f9ac965fa91b0c1e2a9dadfe4
broChenzox <spalnavy@gmail.com>
2022-02-20 14:42:34 +0300

    Added a method to make Hilt to know how to provide an instance of AppDatabase

--- app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
+++ app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
@@ -1,11 +1,16 @@
 package com.example.android.hilt.di
 
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.android.hilt.data.AppDatabase
 import com.example.android.hilt.data.LogDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -14,5 +19,15 @@ object DatabaseModule {
     @Provides
     fun getLogDao(database: AppDatabase): LogDao {
         return database.logDao()
+    }
+
+    @Singleton
+    @Provides
+    fun getAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "logging.db"
+        ).build()
     }
 }
