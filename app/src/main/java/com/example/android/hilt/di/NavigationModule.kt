6bc8246c2d4fa753888b0546edb0611631759fb0
broChenzox <spalnavy@gmail.com>
2022-02-20 15:45:18 +0300

    Added a method to bind navigator

--- app/src/main/java/com/example/android/hilt/di/NavigationModule.kt
+++ app/src/main/java/com/example/android/hilt/di/NavigationModule.kt
@@ -1,5 +1,8 @@
 package com.example.android.hilt.di
 
+import com.example.android.hilt.navigator.AppNavigator
+import com.example.android.hilt.navigator.AppNavigatorImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
@@ -8,5 +11,6 @@ import dagger.hilt.android.components.ActivityComponent
 @InstallIn(ActivityComponent::class)
 abstract class NavigationModule {
 
-
+    @Binds
+    abstract fun bindNavigator(impl: AppNavigatorImpl): AppNavigator
 }
