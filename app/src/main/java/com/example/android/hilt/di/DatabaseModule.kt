49eae6b1c6d5b4e2222bfd095fe1ff010ffecb11
broChenzox <spalnavy@gmail.com>
2022-02-20 14:36:14 +0300

    Added a method to make Hilt to know how to provide an instance of LogDao

--- app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
+++ app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
@@ -1,6 +1,9 @@
 package com.example.android.hilt.di
 
+import com.example.android.hilt.data.AppDatabase
+import com.example.android.hilt.data.LogDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
@@ -8,5 +11,8 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
-
+    @Provides
+    fun getLogDao(database: AppDatabase): LogDao {
+        return database.logDao()
+    }
 }
